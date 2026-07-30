ad5546bb4470e039df6919bf905a7fff1432b46d
Eteran07 <edgar.teran@correo.unimet.edu.ve>
2026-07-30 04:07:01 -0400

    modulo 2 y parte del modulo 3

---
+++ MODULO 3/Listas_Slicing.py
@@ -0,0 +1,7 @@
+# Slicing: Cortando listas
+alumnos = ["Ana", "Bet", "Cris", "Dani", "Eva", "Fer", "Geo", "Hugo"]
+seleccion = alumnos [1:4]
+print (f"lista original: {alumnos}")
+
+print (f"slice (rebanada):{seleccion}")
+
